1ffe3ee371795f6b35b1e9786801895f32a169a9
jujulioed <julioeduardon9@hotmail.com>
2024-09-18 22:54:12 -0300

    Create Login Screen component
     - Created the login screen react component
    
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

---
+++ frontend/src/components/ChatScreen/ChatScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatScreen() {
+    return (
+        <div>
+            <h1>Chat Screen</h1>
+        </div>
+    );
+}
+
+export default ChatScreen;
---
+++ frontend/src/components/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './LoginScreen.css';
+
+function LoginScreen() {
+    return (
+        <div className='div-login'>
+            <div className='div-repository-link'>
+                <button>&lt; Confira o repositório &gt;</button>
+            </div>
+
+            <div className='div-register-button'>
+                <h3>Não possui conta?</h3>
+                <button>Registrar</button>
+            </div>
+            <h1>Simple Chat</h1>
+
+            <form className='form-login' onSubmit=''>
+                <div className='field'>
+                    <label htmlFor="email">EMAIL</label>
+                    <input 
+                        type="email"
+                        placeholder='Insira seu email'
+                        name='email'
+                        className='form-field'
+                        required
+                    />
+                </div>
+                <div className='field'>
+                    <label htmlFor="email">SENHA</label>
+                    <input 
+                        type="password"
+                        placeholder='Insira sua senha'
+                        name='password'
+                        className='form-field'
+                        required
+                    />
+                </div>
+
+                <button type='submit' className='button-login'>Log in</button>
+
+            </form>
+        </div>
+    );
+}
+
+export default LoginScreen;
